3e2916e66683aff119228fa06e84addea1e85e9a
iuSaiba <intiseryusrasaiba@gmail.com>
2021-08-08 12:45:56 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,10 @@
+var applePrice = 90;
+var potatoPrice = 120; 
+var totalPrice = applePrice + potatoPrice;
+var priceUntotal = applePrice - potatoPrice;
+
+console.log(applePrice)
+console.log(potatoPrice)
+console.log(applePrice + potatoPrice)
+console.log(totalPrice) 
+console.log(priceUntotal)
